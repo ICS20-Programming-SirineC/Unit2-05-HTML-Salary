84c40159179fc32a5111ab1eaecbcd5ca1f49ab8
sirinec12345 <122186277+sirinec12345@users.noreply.github.com>
2023-03-22 02:47:32 +0000

    Fixed errors

--- js/script.js
+++ js/script.js
@@ -6,22 +6,22 @@
 
 // Hours Worked
 function calculateSalary() {
-	let hoursWorked = parseFloat(document.getElementById("hours").value);
+   let hoursWorked = parseFloat(document.getElementById("hours").value);
 
-  // Hourly Rate
-	let hourlyRate = parseFloat(document.getElementById("rate").value);
-	let grossSalary = hoursWorked * hourlyRate;
-  
-  //Taxes
-	let tax = grossSalary * 0.185;
-	let takeHomeSalary = grossSalary - tax;
-  
-  //Rounding Decimal
-	let num_decimals = 2;
-	let base = 10 ** num_decimals;
-	takeHomeSalary = Math.round(takeHomeSalary * base) / base;
-	tax = Math.round(tax * base) / base;
+   // Hourly Rate
+   let hourlyRate = parseFloat(document.getElementById("rate").value);
+   let grossSalary = hoursWorked * hourlyRate;
 
-  //Result display
-	document.getElementById("result").innerHTML = "Your pay will be: $" + takeHomeSalary.toFixed(2) + "<br>Amount of tax the government will take: $" + tax.toFixed(2);
+   //Taxes
+   let tax = grossSalary * 0.185;
+   let takeHomeSalary = grossSalary - tax;
+
+   //Rounding Decimal
+   let num_decimals = 2;
+   let base = 10 ** num_decimals;
+   takeHomeSalary = Math.round(takeHomeSalary * base) / base;
+   tax = Math.round(tax * base) / base;
+
+   //Result display
+   document.getElementById("result").innerHTML = "Your pay will be: $" + takeHomeSalary.toFixed(2) + "<br>Amount of tax the government will take: $" + tax.toFixed(2);
 }
